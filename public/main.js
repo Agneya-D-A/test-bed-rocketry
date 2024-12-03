6ffb3c84501b05a392afd66da1d8e27a1a5c1f6e
Agneya D A <agneya181222da@gmail.com>
2024-12-03 20:32:10 +0530

    Project runs in its own window

--- public/main.js
+++ public/main.js
@@ -1,1 +1,37 @@
 const {app, BrowserWindow} = require('electron');
+
+let wind;
+
+function createWindow(){
+    wind = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: 'rocketry_icon.ico',
+        webPreferences: {
+            nodeIntegration: true,  // For accessing Node.js APIs if needed
+            contextIsolation: false,
+        }
+    });
+
+    wind.loadURL('http://localhost:3000');
+
+    wind.on('closed',()=>{
+        wind = null;
+    })
+}
+
+app.on('ready',()=>{
+    createWindow();
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+  
+app.on('activate', () => {
+    if (wind === null) {
+        createWindow();
+    }
+});
